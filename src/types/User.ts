b6cefc5cf64f4b7b9d4ae9b283a4c53ca77b6871
logicshrey <shreyasg24comp@student.mes.ac.in>
2025-03-17 18:55:22 +0530

    missing username fixed

--- src/types/User.ts
+++ src/types/User.ts
@@ -1,8 +1,15 @@
+interface Badge {
+  id: string;
+  name: string;
+  image: string;
+}
+
 export interface User {
   id: string;
   username: string;
   email: string;
   level: number;
   points: number;
+  badges: Badge[];
   // ... any other properties your user might have
 } 
